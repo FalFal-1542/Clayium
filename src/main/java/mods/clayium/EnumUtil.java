ae7d5d57534935b87c9165b6cdd19d3c05b6bdc4
FalFal <66197124+FalFal-1542@users.noreply.github.com>
2022-11-15 04:26:01 +0900

    Delete src directory

--- src/main/java/mods/clayium/EnumUtil.java
+++
@@ -1,9 +0,0 @@
-package mods.clayium;
-
-import mods.clayium.item.common.ClayiumMaterial;
-import mods.clayium.item.common.ClayiumShape;
-
-public class EnumUtil {
-    public static final ClayiumShape[] RESOURCE_TYPES = ClayiumShape.values();
-    public static final ClayiumMaterial[] PRIMARY_RESOURCES = ClayiumMaterial.values();
-}
